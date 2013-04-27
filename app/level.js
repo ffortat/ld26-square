fbc415431c0548847c2865cb346ffe7bf4ff83af
MaxguN <florent.fortat@maxgun.fr>
2013-04-27 17:30:57 +0200

    Improved tile display Added placeholders for square and circles

--- app/level.js
+++ app/level.js
@@ -9,6 +9,7 @@ function Level(name) {
 		h : 480
 	};
 
+	this.tiles = {};
 	this.tilesets = [];
 	this.layers = [];
 	this.width = 0;
@@ -31,17 +32,17 @@ Level.prototype.init = function(level) {
 	level.tilesets.forEach(function (tileset, index) {
 		this.tilesets[index] = {
 			image : new Image(),
-			tiles : [null],
 			width : tileset.tilewidth,
 			height : tileset.tileheight
 		};
 
 		for (var i = 0; i < tileset.imageheight; i += tileset.tileheight) {
 			for (var j = 0; j < tileset.imagewidth; j += tileset.tilewidth) {
-				this.tilesets[index].tiles.push({
+				this.tiles[tileset.firstgid + i / tileset.tileheight * (tileset.imagewidth / tileset.tilewidth) + j / tileset.tilewidth] = {
 					x : j,
-					y : i
-				});
+					y : i,
+					set : index
+				};
 			}
 		}
 
@@ -72,17 +73,17 @@ Level.prototype.tick = function(length) {
 
 Level.prototype.draw = function() {	
 	if (this.loaded) {
-		var tileset = this.tilesets[0];
-		var width = tileset.width;
-		var height = tileset.height;
-
 		this.layers.forEach(function (layer) {
 			if (layer.visible) {
-				layer.data.forEach(function (tile, index) {
-					if (tile > 0) {
+				layer.data.forEach(function (tileid, index) {
+					if (tileid > 0) {
+						var tile = this.tiles[tileid];
+						var tileset = this.tilesets[tile.set];
+						var width = tileset.width;
+						var height = tileset.height;
 						var x = index % this.width * this.tile.width - this.window.x;
 						var y = Math.floor(index / this.width) * this.tile.height - this.window.y;
-						context.drawImage(tileset.image, tileset.tiles[tile].x, tileset.tiles[tile].y, width, height, x, y, width, height);
+						context.drawImage(tileset.image, tile.x, tile.y, width, height, x, y, width, height);
 					}
 				}, this);
 			}
